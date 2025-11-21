96d6a6613cd306f56bcc88dd712504e38505df1c
Yuunichi <43927156+Yuunichi@users.noreply.github.com>
2025-11-20 22:47:31 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -7,4 +7,33 @@ nav_ul.children[1].textContent = "Hola, " + config.nombre;
 
 document.getElementsByName("name")[1].textContent = config.buscar; // Texto del boton de buscar
 
-// Body
+// Section
+
+directorio = document.getElementById("directorio");
+let i=0;
+perfiles.forEach( perfil => {
+    // div contenedores
+    entrada = document.createElement("div");
+    entrada.className = "entrada_directorio";
+    entrada.classList.add("index-" + i);
+    i++;
+    // foto
+    foto_perfil = document.createElement("img");
+    foto_perfil.src = perfil.imagen;    
+    // nombre
+    nombre = document.createElement("p");
+    nombre.textContent = perfil.nombre;
+    // cedula
+    cedula = document.createElement("p");
+    cedula.textContent = perfil.ci;
+    // Se añaden como hijos del div correspondientes    
+    entrada.append(foto_perfil,nombre,cedula);
+    directorio.append(entrada);
+});
+
+// Footer
+
+copy = document.createElement("p");
+copy.textContent = config.copyRight;
+footer = document.getElementsByTagName("footer")[0];
+footer.append(copy);
