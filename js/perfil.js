65a88b9dcc5a3013ea298d1d313c2515c7f36f8e
Yuunichi <43927156+Yuunichi@users.noreply.github.com>
2025-11-22 23:36:38 -0400

    Agregado los datos del perfil en perfil.html desde perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -1,31 +1,41 @@
-perfil_img = document.getElementById("perfil-img");
+let perfil_img = document.getElementById("perfil-img");
 perfil_img.src = perfiles[0].imagen;
+let perfilElement = document.getElementById("perfil");
 
-perfil = document.getElementById("perfil");
+let perfil_nombre = document.createElement("h1");
+perfil_nombre.textContent = perfil.nombre;
 
-perfil_nombre = document.createElement("h1");
-perfil_nombre.textContent = perfil_.nombre;
+let descripcion = document.createElement("p");
+let descripcion_italic = document.createElement("i");
+descripcion.append(descripcion_italic);
+descripcion_italic.textContent = perfil.descripcion;
 
-descripcion = document.createElement("p");
-descripcion.textContent = perfil_.descripcion;
+let color = document.createElement("p");
+let libro = document.createElement("p");
+let juego = document.createElement("p");
+let musica = document.createElement("p");
+let lenguajes = document.createElement("p");
+let lenguajes_bold = document.createElement("b");
+let p_email = document.createElement("p");
+let email = document.createElement("a");
 
-color = document.createElement("p");
-libro = document.createElement("p");
-juego = document.createElement("p");
-musica = document.createElement("p");
-lenguajes = document.createElement("p");
-lenguajes = document.createElement("p");
+color.textContent = "Color favorito: " + perfil.color;
+libro.textContent = "Libro favorito: " + perfil.libro;
+juego.textContent = "Juego favorito: " + perfil.video_juego;
+musica.textContent = "Musica favorito: " + perfil.musica;
+lenguajes_bold.textContent = config.lenguajes + ": " + perfil.lenguajes;
+p_email.textContent = config.email;
+email.textContent = perfil.email;
+email.href = "mailto:" + perfil.email;
+email.id = "email";
 
-color.textContent = "Color favorito: " + perfil_.color;
-libro.textContent = "Libro favorito: " + perfil_.libro;
-juego.textContent = "Juego favorito: " + perfil_.juego;
-musica.textContent = "Musica favorito: " + perfil_.musica;
-lenguajes.textContent = config.lenguajes+": " + perfil_.lenguajes;
-
-perfil.append(perfil_nombre);
-perfil.append(descripcion);
-perfil.append(color);
-perfil.append(libro);
-perfil.append(juego);
-perfil.append(musica);
-perfil.append(lenguajes);
+perfilElement.append(perfil_nombre);
+perfilElement.append(descripcion);
+perfilElement.append(color);
+perfilElement.append(libro);
+perfilElement.append(juego);
+perfilElement.append(musica);
+perfilElement.append(lenguajes);
+lenguajes.append(lenguajes_bold);
+p_email.append(email);
+perfilElement.append(p_email);
